884e8692b5727d39c2d33f7837540d96eba6545a
AMB <clasicnew1@gmail.com>
2022-08-25 00:29:17 +0430

    Admin & Main WebSite Template Implemented

---
+++ Src/KalaMarket/KalaMarket.EndPoint/Pages/Admin/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using KalaMarket.EndPoint.Pages.Base;
+namespace KalaMarket.EndPoint.Pages.Admin.Pages
+{
+    public class IndexModel : BasePageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Src/KalaMarket/KalaMarket.EndPoint/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using KalaMarket.EndPoint.Pages.Base;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KalaMarket.EndPoint.Pages
+{
+    public class IndexModel : BasePageModel
+    {
+        public void OnGet()
+        {
+           
+        }
+    }
+}
