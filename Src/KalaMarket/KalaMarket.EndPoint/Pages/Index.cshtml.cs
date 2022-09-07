af4ec8d9d457c3b1263da6eb11942cb78521cc21
AMB <clasicnew1@gmail.com>
2022-09-07 17:47:59 +0430

    fix : Remove Some Test Code

--- Src/KalaMarket/KalaMarket.EndPoint/Pages/Index.cshtml.cs
+++ Src/KalaMarket/KalaMarket.EndPoint/Pages/Index.cshtml.cs
@@ -12,35 +12,12 @@ namespace KalaMarket.EndPoint.Pages
         }
         public async Task OnGet()
         {
-            person person = new person()
-            {
-                id = 2,
-                DateTimeNow = DateTime.Now,
-                name = "reza"
-            };
-            test.list.Add(person);
-           await Logger.LogInformation("User Added {person}", person);
+          
         }
      
     }
 
-    public class test
-    {
-        public static List<person> list = new List<person>()
-        {
-            new person()
-            {
-                name = "amir",
-                DateTimeNow = DateTime.Now,
-                id = 1
-            }
-        };
-    }
+ 
 
-    public class person
-    {
-        public long id { get; set; }
-        public string name { get; set; }
-        public DateTime DateTimeNow { get; set; }
-    }
+    
 }
