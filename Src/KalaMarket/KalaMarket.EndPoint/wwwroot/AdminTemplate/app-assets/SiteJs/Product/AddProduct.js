0a491499a66fef4572f8286a150f806da76ee289
AMB <clasicnew1@gmail.com>
2022-09-25 10:11:55 +0330

    feat(add js) : Add Js File In Root Folder (Endpoint)

---
+++ Src/KalaMarket/KalaMarket.EndPoint/wwwroot/AdminTemplate/app-assets/SiteJs/Product/AddProduct.js
@@ -0,0 +1,124 @@
+﻿
+    //#region Add Jpg Validator
+        jQuery.validator.addMethod("ValidFileExtension",
+            function (value, element, param) {
+                var extension = value.slice(((value.lastIndexOf(".") - 1) + 2));
+                const validExtension = ["png", "jpeg", "jpg", "webp"];
+                var result = validExtension.some((ex) => ex === extension);
+                return result;
+            });
+        jQuery.validator.unobtrusive.adapters.addBool("ValidFileExtension");
+
+        var isInputFeatureAdded = false;
+        var AddProductForm = document.querySelector("#AddProductForm");
+        var btnSubmitProductForm = document.querySelector("#btnAddProduct");
+        var countTagForProduct = 0;
+        AddProductForm.addEventListener("submit",
+            (e) => {
+                console.log("inputValue");
+                console.log(isInputFeatureAdded);
+                if (isInputFeatureAdded === true) {
+                    isInputFeatureAdded = false;
+                    $("#btnAddProduct").prop("disabled", true);
+                } else {
+                    e.preventDefault();
+                    addInputFeatures();
+                    $("#btnAddProduct").click();
+                }
+            });
+ //#endregion /Add Jpg Validator
+
+    //#region Add Feature To Table
+ $("#btnAddFeatures").on("click",
+            function () {
+                var txtDisplayName = $("#Feature_Key").val();
+                var txtValue = $("#Feature_Value").val();
+                if (txtDisplayName == "" || txtValue == "") {
+                    swal.fire(
+                        'هشدار!',
+                        "نام و مقدار را باید وارد کنید",
+                        'warning'
+                    );
+                } else {
+                    if (CheckProductFeatureExist(txtDisplayName) === true) {
+                        swal.fire(
+                            '!خطا',
+                            "ویژگی تکراریست",
+                            'error'
+                        );
+                    } else {
+                        isInputFeatureAdded = false;
+                        $('#tbl_Features tbody').append('<tr> <td>' + txtDisplayName + '</td>  <td>' + txtValue + '</td> <td> <a class="idFeatures btnDelete btn btn-danger"> حذف </a> </td> </tr>');
+                        $("#txtDisplayName").val('');
+                        $("#txtValue").val('');
+                    }
+                }
+     });
+ //#endregion /Add Feature To Table
+
+    //#region   Check Feature Exist In Table Features    
+        function CheckProductFeatureExist(feature) {
+            var dataFeaturesViewModel = $('#tbl_Features tr:gt(0)').map(function () {
+                return {
+                    DisplayName: $(this.cells[0]).text(),
+                };
+            }).get();
+            var result = false;
+            $.each(dataFeaturesViewModel,
+                function (i, val) {
+                    console.log(val.DisplayName);
+                    if (val.DisplayName === feature) {
+                        result = true;
+                    }
+                });
+            return result;
+        }
+//#endregion /Check Feature Exist In Table Features
+
+    //#region Remove Selected Row In Table Features
+        $("#tbl_Features").on('click',
+            '.idFeatures',
+            function () {
+                $(this).closest('tr').remove();
+            });
+//#endregion /Remove Selected Row In Table Features
+
+    //#region Add Hidden Input For Bind Features To BackEnd
+        function addInputFeatures() {
+            var List_Feature = $("#List_Feature");
+            List_Feature.empty();
+            var ShowError = $("#Show_Errors");
+            ShowError.empty();
+            var dataFeaturesViewModel = $('#tbl_Features tr:gt(0)').map(function () {
+                return {
+                    DisplayName: $(this.cells[0]).text(),
+                    Value: $(this.cells[1]).text(),
+                };
+            }).get();
+            if (dataFeaturesViewModel.length === 0) {
+                swal.fire(
+                    'هشدار!',
+                    "باید یک ویژگی وارد کنید",
+                    'warning'
+                );
+            }
+            $.each(dataFeaturesViewModel, 
+                function (i, val) {
+                    List_Feature.append(`<input name="Features[${i}].KeyName" id="Features_${i}__KeyName" type="hidden" data-val="true" value="${val.DisplayName}"/></br>`);
+                    List_Feature.append(`<input name="Features[${i}].KeyValue" id="Features_${i}__KeyValue" type="hidden"  value="${val.Value}"/></br>`);
+                    ShowError.append(`< span class="text-danger" asp - validation -for= "Features[${i}].KeyName" ></span > `);
+                    ShowError.append(`< span class="text-danger" asp - validation -for= "Features[${i}].KeyValue" ></span > `);
+                    isInputFeatureAdded = true;
+                });
+            var formProduct = $('#AddProductForm');
+            //sets up the validate
+            formProduct.validate();
+            //checks if it's valid
+            if (formProduct.valid()) {
+            //horray it's valid
+                formProduct.prop("disabled", true);
+                formProduct.submit();
+            }
+
+        }
+//#endregion /Add Hidden Input For Bind Features To BackEnd
---
+++ Src/KalaMarket/KalaMarket.EndPoint/wwwroot/AdminTemplate/app-assets/SiteJs/User/RemoveUser/ChangeStatusRemovedUser.js
@@ -0,0 +1,30 @@
+﻿function changeStatus(id) {
+    $.ajax({
+        url: `RemovedUsers/ChangeStatus?id=${id}`,
+        method: "Put",
+        contentType: `application/json; charset=utf-8`,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN",
+                $(`input:hidden[name="__RequestVerificationToken"]`).val());
+        },
+    }).done(function (result) {
+        Swal.fire({
+            icon: 'success',
+            title: 'وضعیت کاربر انتخاب شده\n تغیر پیدا کرد',
+            timer: 2500,
+            confirmButtonColor: '#3085d6',
+        }).then(() => {
+            location.reload(true)
+        });
+
+    }).fail(function (result) {
+        Swal.fire({
+            //icon: 'question',
+            icon: "error",
+            html: `<h4 class="text-danger">${result.responseJSON.message}</h4>`,
+            showCloseButton: true,
+            confirmButtonText: "بستن",
+            confirmButtonColor: '#3085d6',
+        })
+    });
+}
